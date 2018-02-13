843228b647b9ca2d33c4c9c41ef5a5ec29ef239c
Yapcoin Project <yapcoin.project@gmail.com>
2018-02-14 00:43:23 +0500

    Yapcoin: hotfix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -14,6 +14,9 @@
 
 #include "chainparamsseeds.h"
 
+#include "arith_uint256.h"
+#include <boost/assign/list_of.hpp>
+
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     CMutableTransaction txNew;
